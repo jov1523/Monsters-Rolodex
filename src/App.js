c5a590b85626b1fab837b9a0362b7f0dcd135002
unknown <byohannes767@gmail.com>
2025-10-24 11:27:42 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component.jsx';
+import './App.css';
+
+class App extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(respons => respons.json())
+      .then(users => this.setState({monsters: users}));
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <CardList name="jovani">
+          {this.state.monsters.map(monster => (<h1 key={monster.id}>{monster.name}</h1>))}
+        </CardList>
+        
+      </div>
+    );
+    
+  }
+}
+
+
+export default App;
